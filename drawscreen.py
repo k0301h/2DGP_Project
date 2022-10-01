831171efe0b484ef6f5a40c88c0e58affedaff1d
inho.go(2019180003) <58318769+k0301h@users.noreply.github.com>
2022-10-01 16:45:21 +0900

    점프 올라가는거 구현 및 스테이지 크기 측정
    25x25
    중력 구현 하면 점프 구현 완료

--- drawscreen.py
+++ drawscreen.py
@@ -1,6 +1,8 @@
 from pico2d import *
 from characterclass import *
 from map_floor import *
+import time
+import threading
 
 character = CHARACTER()
 open_canvas()
@@ -13,18 +15,33 @@ FLOOR_stage_I = load_image('floor_cave.png')
 # Jump_Key_State = False
 # Can_Jump = True
 Gravity = 3.0
-JumpSpeed = 30
+JumpSpeed = 20
+
+shift_on = 0
+
+start = 0
+end = 0
+
+def start_time():
+    global start
+    start = time.time()
+
+def end_timer():
+    global end
+    end = time.time()
 
 # 느낌만 구현 나중에 맵 구체적으로 계획후 배열 제작
 def draw_map_floor():
-    for index in range(0, 8 * 4):
+    for index in range(0, 25 * 25):
         if map_floor_array[index] == 1:
-            FLOOR_stage_I.clip_draw(0, 400, 100, 100, index % 8 * 100, 600 - index // 8 * 100)
+            FLOOR_stage_I.clip_draw(0, 448, 64, 64, index % 25 * 64, 600 - index // 25 * 64)
     
 
 def draw_character():
     # global Jump_Key_State
     # global Can_Jump
+    global shift_on
+    global JumpSpeed
 
     clear_canvas()
     BG_stage_I.draw(400, 300)
@@ -47,7 +64,6 @@ def draw_character():
             elif event.key == SDLK_RIGHT:
                 character.Action = 1
                 character.DIRECTION = 0
-                character.X += 5
             elif event.key == SDLK_DOWN:
                 character.Action = 2
             elif event.key == SDLK_LEFT:
@@ -56,7 +72,8 @@ def draw_character():
             elif event.key == SDLK_LALT:
                 # Jump_Key_State = True
                 character.Action = 4
-                character.Y += 10
+            elif event.key == SDLK_LSHIFT:
+                shift_on = 1
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_UP:
                 character.Action = 0
@@ -69,28 +86,39 @@ def draw_character():
             elif event.key == SDLK_LALT:
                 # Jump_Key_State = False
                 character.Action = 0
+                JumpSpeed = 20
+            elif event.key == SDLK_LSHIFT:
+                shift_on = 0
 
     if character.Action == 0:
         character.MotionIndex = 0
     elif character.Action == 1:
         character.MotionIndex = (character.MotionIndex + 1) % 8
-        character.X += 5
+        if shift_on == 0:
+            character.X += 5
+        else:
+            character.X += 15
     elif character.Action == 2:
         if character.MotionIndex < 18:
             character.MotionIndex = (character.MotionIndex + 1) % 16 % 3 + 16
     elif character.Action == 3:
         character.MotionIndex = (character.MotionIndex + 1) % 8
-        character.X -= 5
+        if shift_on == 0:
+            character.X -= 5
+        else:
+            character.X -= 15
     elif character.Action == 4:
         character.MotionIndex = (character.MotionIndex + 1) % 16 % 8 + 16 * 9
+        Jump()
 
 
 
-# def Jump():
-#     global DropSpeed
-#     DropSpeed += Gravity
-#
-#     if
+def Jump():
+    global JumpSpeed
+    global character
+    JumpSpeed -= Gravity
+    if JumpSpeed > 0:
+        character.Y += JumpSpeed
 
 while (1):
     draw_character()
---
+++ main.py
@@ -0,0 +1,3 @@
+from drawscreen import *
+
+draw_character()
