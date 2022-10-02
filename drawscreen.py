d74699b841384b7026dcc993c660da0cf3373869
inho.go(2019180003) <58318769+k0301h@users.noreply.github.com>
2022-10-02 16:52:17 +0900

    아래점프 구현
    but 여러키를 동시에 입력했을때 부자연 스러움

--- drawscreen.py
+++ drawscreen.py
@@ -19,6 +19,7 @@ JumpSpeed = 20
 
 shift_on = True
 Jump_Key_State = False
+Down_Jump_state = False
 
 start = 0
 end = 0
@@ -44,17 +45,22 @@ def draw_character():
     global shift_on
     global JumpSpeed
     global Jump_Key_State
+    global Down_Jump_state
 
     clear_canvas()
-    BG_stage_I.draw(400, 300)
-    draw_map_floor()
+    BG_stage_I.draw(200, 200)
+    BG_stage_I.draw(600, 200)
+    BG_stage_I.draw(200, 600)
+    BG_stage_I.draw(600, 600)
+
+    # draw_map_floor()
     if character.DIRECTION == 0:
       character_I.clip_draw(character.MotionIndex % 16 * 128, 1918 - 128 * (character.MotionIndex // 16), 128, 128, character.X, character.Y)
     elif character.DIRECTION == 1:
       character_reverse_I.clip_draw(1918 - character.MotionIndex % 16 * 128, 1918 - 128 * (character.MotionIndex // 16), 128, 128, character.X, character.Y)
     # character_I.clip_draw(character.MotionIndex % 16 * 128, 1918 - 128 * (character.MotionIndex // 16), 128, 128, character.X, character.Y)
     update_canvas()
-    delay(0.075)
+    delay(0.05)
     events = get_events()
 
     for event in events:
@@ -64,15 +70,20 @@ def draw_character():
             if event.key == SDLK_UP:
                 pass
             elif event.key == SDLK_RIGHT:
-                character.Action = 1
-                character.DIRECTION = 0
+                if character.Action != 2:
+                    character.Action = 1
+                    character.DIRECTION = 0
             elif event.key == SDLK_DOWN:
                 character.Action = 2
             elif event.key == SDLK_LEFT:
-                character.DIRECTION = 1
-                character.Action = 3
+                if character.Action != 2:
+                    character.DIRECTION = 1
+                    character.Action = 3
             elif event.key == SDLK_LALT:
-                Jump_Key_State = True
+                if character.Action == 2 and not Jump_Key_State:
+                    Down_Jump_state = True
+                else:
+                    Jump_Key_State = True
             elif event.key == SDLK_LSHIFT:
                 shift_on = True
         elif event.type == SDL_KEYUP:
@@ -82,29 +93,40 @@ def draw_character():
                 character.Action = 0
             elif event.key == SDLK_LEFT and character.Action == 3:
                 character.Action = 0
-            elif event.key == SDLK_LALT and Jump_Key_State:
+            elif event.key == SDLK_LALT and (Jump_Key_State or Down_Jump_state):
                 Jump_Key_State = False
+                Down_Jump_state = False
                 JumpSpeed = 20
             elif event.key == SDLK_LSHIFT:
                 shift_on = False
 
+    Motion()
+
+
+
+def Motion():
+    global character
     if Jump_Key_State:
         Jump()
+    elif Down_Jump_state:
+        Down_Jump()
+
 
     if character.Action == 0:
-        character.MotionIndex = 0
+        if not Jump_Key_State:
+            character.MotionIndex = 0
     elif character.Action == 1:
-        if Jump_Key_State == False:
+        if not Jump_Key_State:
             character.MotionIndex = (character.MotionIndex + 1) % 8
         if shift_on == 0:
             character.X += 5
         else:
             character.X += 15
     elif character.Action == 2:
-        if character.MotionIndex < 18 and Jump_Key_State == False:
+        if character.MotionIndex < 18 and not Jump_Key_State:
             character.MotionIndex = (character.MotionIndex + 1) % 16 % 3 + 16
     elif character.Action == 3:
-        if Jump_Key_State == False:
+        if not Jump_Key_State:
             character.MotionIndex = (character.MotionIndex + 1) % 8
         if shift_on == 0:
             character.X -= 5
@@ -112,12 +134,20 @@ def draw_character():
             character.X -= 15
 
 def Jump():
-    global JumpSpeed
     global character
+    global JumpSpeed
     character.MotionIndex = (character.MotionIndex + 1) % 16 % 8 + 16 * 9
     JumpSpeed -= Gravity
     if JumpSpeed > 0:
         character.Y += JumpSpeed
+
+def Down_Jump():
+    global character
+    global JumpSpeed
+    character.MotionIndex = (character.MotionIndex + 1) % 16 % 8 + 16 * 9
+    JumpSpeed -= Gravity
+    if JumpSpeed > 0:
+        character.Y -= JumpSpeed
 
 while (1):
     draw_character()
