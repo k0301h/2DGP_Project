6c13bb9d83053c988f0afc51f0e9bf23950cf395
inhogo(2019180003) <dlsghdmsqlc@naver.com>
2022-10-20 23:55:33 +0900

    몬스터 움직임 구현

--- monsterclass.py
+++ monsterclass.py
@@ -28,24 +28,24 @@ class MONSTER(UNIT):
         self.Y = 200
 
     def Conflict_checking(self, mode, move): # mode : x,y충돌 검사 , move : 다음에 움직일 크기
-        character_index_x = int((self.X + move) // 60)
-        character_index_y = int((HEIGHT - (self.Y + move)) // 60)
-
-        if mode == 1:       # Y충돌 체크
+        if mode == 1:  # Y충돌 체크
+            character_index_x = int(self.X // 60)
+            character_index_y = int((HEIGHT - (self.Y + move)) // 60)
             for index_x in range(character_index_x - 1, character_index_x + 2):
                 for index_y in range(character_index_y - 2, character_index_y + 3):
-                    if 0 <= index_x < map_size and  0 <= index_y < map_size and\
-                            2 <= map_floor_array[index_y][index_x] <= 29 and\
-                            abs(self.X - index_x * 60) < 60 and abs(self.Y + move - (HEIGHT - index_y * 60)) <= 65:
+                    if 0 <= index_x < map_size and 0 <= index_y < map_size and \
+                            2 <= map_floor_array[index_y][index_x] <= 29 and \
+                            abs(self.X - index_x * 60) <= 55 and abs(self.Y + move - (HEIGHT - index_y * 60)) <= 60:
                         return False
 
-        elif mode == 2:     # X충돌 체크
-            for index_y in range(character_index_y - 1, character_index_y + 1):
+        elif mode == 2:  # X충돌 체크
+            character_index_x = int((self.X + move) // 60)
+            character_index_y = int((HEIGHT - self.Y) // 60)
+            for index_y in range(character_index_y - 1, character_index_y + 2):
                 for index_x in range(character_index_x - 2, character_index_x + 3):
-                    if 0 <= index_x < map_size and 0 <= index_y < map_size and\
+                    if 0 <= index_x < map_size and 0 <= index_y < map_size and \
                             2 <= map_floor_array[index_y][index_x] <= 29 and \
-                            abs(self.Y - (HEIGHT - index_y * 60)) < 65 and abs(self.X + move - index_x * 60) <= 60:
-                        print(abs(self.X + move - index_x * 60))
+                            abs(self.Y - (HEIGHT - index_y * 60)) < 60 and abs(self.X + move - index_x * 60) <= 55:
                         return False
         return True
 
@@ -61,15 +61,22 @@ class MONSTER(UNIT):
 
     def Motion(self):
         if self.Action == 0:
-            self.MotionIndex = 0
+            self.MotionIndex = (self.MotionIndex + 1) % 4
+            if self.Conflict_checking(2, 3):
+                self.X += 3
 
         self.gravity()
 
-    def draw_monster(self, monster_image):
+    def draw_monster(self, monster_image, monster_grid):
+        monster_grid.clip_draw(int(self.MotionIndex) % 4 * 128,
+                                  544 - 128 * (int(self.MotionIndex) // 4 + 1),
+                                  128, 128, self.X - characterclass.CHARACTER.camera_move_x + 30,
+                                  self.Y - characterclass.CHARACTER.camera_move_y - 30,
+                                  60, 60)
         if self.DIRECTION == 0:
-            monster_image.clip_draw(int(self.MotionIndex) % 4 * 130,
-                                  544 - 95 * (int(self.MotionIndex) // 4 - 30),
-                                  130, 95, self.X - characterclass.CHARACTER.camera_move_x + 30,
+            monster_image.clip_draw(int(self.MotionIndex) % 4 * 128,
+                                  544 - 128 * (int(self.MotionIndex) // 4 + 1),
+                                  128, 128, self.X - characterclass.CHARACTER.camera_move_x + 30,
                                   self.Y - characterclass.CHARACTER.camera_move_y - 30,
                                   60, 60)
         # elif self.DIRECTION == 1:
--- play_state.py
+++ play_state.py
@@ -12,15 +12,17 @@ character_reverse_I = None
 BG_stage_I = None
 FLOOR_stage_I = None
 test_monster_image = None
+test_monster_grid = None
 
 def enter():
-    global main_character, character_I, character_reverse_I, BG_stage_I, FLOOR_stage_I, test_monster, test_monster_image, main_character_grid
+    global main_character, character_I, character_reverse_I, BG_stage_I, FLOOR_stage_I, test_monster, test_monster_image, main_character_grid, test_monster_grid
     test_monster = MONSTER()
     test_monster.Place()
     main_character = CHARACTER()
     main_character.Place()
     main_character_grid = load_image('./Textures/Entities/char_yellow_full_grid.png')
     test_monster_image = load_image('./Textures/Entities/Monsters/snake.png')
+    test_monster_grid = load_image('./Textures/Entities/Monsters/snake_grid.png')
     character_I = load_image('./Textures/char_yellow.png')
     character_reverse_I = load_image('./Textures/r_char_yellow.png')
     BG_stage_I = load_image('./Textures/bg_cave.png')
@@ -36,12 +38,13 @@ def exit():
 def update():
     main_character.key_down()
     main_character.Motion()
+    test_monster.Motion()
 
 def draw():
     pico2d.clear_canvas()
     draw_map_floor(BG_stage_I, FLOOR_stage_I, main_character)
     main_character.draw_character(character_I, character_reverse_I, main_character_grid)
-    test_monster.draw_monster(test_monster_image)
+    test_monster.draw_monster(test_monster_image, test_monster_grid)
     main_character.draw_UI()
     pico2d.update_canvas()
 
