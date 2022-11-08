b11983c0a0a7266ff80c92911de90d87fa538074
inho.go(2019180003) <dlsghdmsqlc@naver.com>
2022-11-08 14:11:08 +0900

    title_state start motion 완성

--- title_state.py
+++ title_state.py
@@ -29,7 +29,8 @@ running = True
 game_start = False
 
 radian = 0
-
+move = 2
+end_move = 0
 
 def enter():
     print('enter title_state')
@@ -80,12 +81,16 @@ def exit():
 
 def update():
     # print('update title_state')
-    global running, radian
+    global running, radian, move, end_move
 
     if radian <= 6.28 and game_start:
-        radian += 0.03
+        radian += 0.06
         delay(0.03)
-
+        move = -move
+    elif end_move <= HEIGHT * 3 / 5 and game_start:
+        end_move += HEIGHT / 100
+        delay(0.06)
+        move = -move
 
 def draw():
     # print('draw title_state')
@@ -95,23 +100,36 @@ def draw():
         main_image2.clip_draw(0, 0, 1024, 1024, WIDTH / 3, HEIGHT / 3, HEIGHT, HEIGHT)
         main_image0.clip_draw(126, 0, 130, 130, (7 * WIDTH) / 11, HEIGHT / 5, HEIGHT / 20, HEIGHT / 20)
     elif game_start:
-        sub_back_image0.clip_draw(0, 0, 512, 512, WIDTH / 2, HEIGHT / 2, WIDTH, HEIGHT)
-        sub_image0.clip_draw(0, 0, 1024, 256, WIDTH / 2, HEIGHT / 5, (2 * WIDTH) / 3, HEIGHT / 4)
+        sub_back_image0.clip_draw(0, 0, 512, 512, WIDTH / 2 + move, HEIGHT / 2, WIDTH, HEIGHT)
+        sub_image0.clip_draw(0, 0, 1024, 256, WIDTH / 2 + move, HEIGHT / 5, (2 * WIDTH) / 3, HEIGHT / 4)
 
         select_image.clip_draw(895, 1280 - 730, 360, 55, select_menu_x - WIDTH / 6, select_menu_y, 210, 30)
         select_image.clip_composite_draw(895, 1280 - 730, 360, 55, 0, 'h', select_menu_x + WIDTH / 6, select_menu_y,
                                          210, 30)
+        sub_image1.clip_draw(0, 0, 1024, 1024, WIDTH / 2 + move, HEIGHT / 2, HEIGHT, HEIGHT)
 
-        sub_image1.clip_draw(0, 0, 1024, 1024, WIDTH / 2, HEIGHT / 2, HEIGHT, HEIGHT)
-        sub_image2.clip_draw(0, 0, 1280, 1080, WIDTH / 2, HEIGHT / 2, (4 * HEIGHT) / 3 , HEIGHT)
-        sub_image3.clip_draw(0, 0, 768, 1080, WIDTH / 5, HEIGHT / 2, HEIGHT, HEIGHT)
-        sub_image3.clip_draw(768, 0, 768, 1080, (4 * WIDTH) / 5, HEIGHT / 2, HEIGHT, HEIGHT)
-        sub_image4.clip_draw(0, 0, 512, 1080, HEIGHT / 4, HEIGHT / 2, HEIGHT / 2, HEIGHT)
-        sub_image4.clip_draw(512, 0, 512, 1080, WIDTH - HEIGHT / 4, HEIGHT / 2, HEIGHT / 2, HEIGHT)
+        if end_move < HEIGHT:
+            main_body_image.clip_composite_draw(0, 0, 512, 512, 0, '', WIDTH / 2 + move, HEIGHT / 2 - HEIGHT / 5 - end_move,
+                                                HEIGHT / 2, HEIGHT / 2)
+            if radian < 6.28:
+                main_door_image.clip_composite_draw(0, 0, 1024, 1024, radian / 2, '', WIDTH / 2 + move, HEIGHT / 2 - end_move, HEIGHT * 6 / 7,
+                                                    HEIGHT * 6 / 7)
+            else:
+                main_door_image.clip_composite_draw(0, 0, 512, 1024, radian / 2, '', WIDTH / 2 + move + end_move + HEIGHT * 2 / 9,
+                                                    HEIGHT / 2, HEIGHT * 3 / 7,
+                                                    HEIGHT * 6 / 7)
+                main_door_image.clip_composite_draw(512, 0, 512, 1024, radian / 2, '', WIDTH / 2 + move - end_move - HEIGHT * 2 / 9,
+                                                    HEIGHT / 2, HEIGHT * 3 / 7,
+                                                    HEIGHT * 6 / 7)
+            main_head_image.clip_composite_draw(0, 0, 512, 512, -radian, '', WIDTH / 2 + move, HEIGHT / 2 - end_move, HEIGHT / 2,
+                                                HEIGHT / 2)
 
-        main_body_image.clip_composite_draw(0, 0, 512, 512, 0, '', WIDTH / 2, HEIGHT / 2 - HEIGHT / 5, HEIGHT / 2, HEIGHT / 2)
-        main_door_image.clip_composite_draw(0, 0, 1024, 1024, radian / 2, '', WIDTH / 2, HEIGHT / 2, HEIGHT / 2, HEIGHT / 2)
-        main_head_image.clip_composite_draw(0, 0, 512, 512, -radian, '', WIDTH / 2, HEIGHT / 2, HEIGHT / 4, HEIGHT / 4)
+        sub_image2.clip_draw(0, 0, 1280, 1080, WIDTH / 2 + move, HEIGHT / 2, (4 * HEIGHT) / 3 , HEIGHT)
+        sub_image3.clip_draw(0, 0, 768, 1080, WIDTH / 5 + move, HEIGHT / 2, HEIGHT, HEIGHT)
+        sub_image3.clip_draw(768, 0, 768, 1080, (4 * WIDTH) / 5 + move, HEIGHT / 2, HEIGHT, HEIGHT)
+        sub_image4.clip_draw(0, 0, 512, 1080, HEIGHT / 4 + move, HEIGHT / 2, HEIGHT / 2, HEIGHT)
+        sub_image4.clip_draw(512, 0, 512, 1080, WIDTH - HEIGHT / 4 + move, HEIGHT / 2, HEIGHT / 2, HEIGHT)
+
     pico2d.update_canvas()
 
 def handle_events():
